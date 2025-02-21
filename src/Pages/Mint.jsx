c79ff06305f679c176b49ab16a49699eeaf947fb
Kien Lam <lorine93skilesvca@hotmail.com>
2025-02-21 16:53:43 +0100

    start

--- src/Pages/Mint.jsx
+++ src/Pages/Mint.jsx
@@ -316,7 +316,7 @@ const Mint = () => {
                                             <p><TiWarning /> 1 per WL NFT</p>
                                             {/*  Mint Button works when eligibility is true, mintedCount is less than 300, and isMinting is false, wallet balance is greater than or equal to 15 SUI or if the wallet has minted one of our collection already  */}
                                             <button
-                                              disabled={true}
+                                              disabled={!isEligible || mintedCount >= 300 || !isMinting || formattedBalance < 15}
                                                 onClick={handleMint}
                                             >
                                                 Mint Now
